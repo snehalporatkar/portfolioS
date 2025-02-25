355db071618e9ef1342a555b21f7fb343248885e
snehalporatkar <snehalporatkar10g@gmail.com>
2025-02-25 21:43:46 +0530

    next change

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -20,7 +20,7 @@ class Footer extends Component {
 
            <ul className="copyright">
               <li>&copy; Copyright 2025 SB</li>
-              <li>Design by <a title="Styleshout" href="#">Snehal Bhagwate</a></li>
+              <li>Design by <a title="Styleshout">Snehal Bhagwate</a></li>
            </ul>
 
         </div>
